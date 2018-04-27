5a77346e60bc38b18f48acf5c20db5b3f3e1ddca
bonpapajosie <35196773+bonpapajosie@users.noreply.github.com>
2018-04-27 16:47:37 +0200

    balance.ts

--- src/app/components/balls/balls.component.ts
+++ src/app/components/balls/balls.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-balls',
   templateUrl: './balls.component.html',
@@ -12,17 +11,7 @@ export class BallsComponent implements OnInit {
   selectedball: number;
   nbballs: number[];
   balltab: number[];
-  msg: String;
-<<<<<<< HEAD
   ballmsg: {};
-=======
-<<<<<<< HEAD
-  ballmsg: {};
-=======
->>>>>>> f6d5c293fd236d058c2f7a4008371eaa8f92b009
->>>>>>> 0eaaa786dbc5e6b2a92f4ac0c89188a4ac848d61
-
-
   constructor() {
     this.nbballs = [1, 2, 3, 4, 5, 6, 7, 8];
     this.defaultval = 2;
@@ -37,21 +26,17 @@ export class BallsComponent implements OnInit {
   }
 
   ballcheck() {
-    this.balltab = [];
-    this.nbballs.forEach(ballnb => {
-      if (ballnb === this.selectedball){
-        this.balltab.push(this.selectedval);
-      } else {
-        this.balltab.push(this.defaultval);
-      }
-    });
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 0eaaa786dbc5e6b2a92f4ac0c89188a4ac848d61
-    this.ballmsg = this.comparateur(this.balltab);
-    alert('la ball ' + this.ballmsg['numeroballon'] + ' est la plus lourd');
-    alert('la fonction à tourner ' + this.ballmsg['nbfoisrelancer'] + ' fois, pour trouver le resultat');
+      this.balltab = [];
+      this.nbballs.forEach(ballnb => {
+        if (ballnb === this.selectedball){
+          this.balltab.push(this.selectedval);
+        } else {
+          this.balltab.push(this.defaultval);
+        }
+      this.ballmsg = this.comparateur(this.balltab);
+      alert('la ball ' + this.ballmsg['numeroballon'] + ' est la plus lourd');
+      alert('la fonction à tourner ' + this.ballmsg['nbfoisrelancer'] + ' fois, pour trouver le resultat');
+    }
   }
 
   comparateur(input) {
@@ -79,29 +64,6 @@ export class BallsComponent implements OnInit {
               'nbfoisrelancer' : (i + 1)
             };
             return output;
-<<<<<<< HEAD
-=======
-=======
-    this.msg = 'la ball ' + this.comparateur(this.balltab) + ' est la plus lourd';
-
-    alert(this.msg);
-  }
-
-  comparateur(input) {
-    console.log(input);
-    let i: number;
-    let c: number;
-    for (i = 0, c = (this.nbballs.length - 1); i <= c; i++) {
-      if (input[i] !== input[i + 1]) {
-        if ((i + 1) === (this.nbballs.length - 1)) {
-          return this.nbballs[i + 1];
-        } else {
-          if (input[i + 1] !== input[i + 2]) {
-            return this.nbballs[i + 1];
-          } else {
-            return this.nbballs[i];
->>>>>>> f6d5c293fd236d058c2f7a4008371eaa8f92b009
->>>>>>> 0eaaa786dbc5e6b2a92f4ac0c89188a4ac848d61
           }
         }
       }
--- src/app/components/balls/comparateur.ts
+++ src/app/components/balls/comparateur.ts
@@ -1,8 +1,4 @@
 export function comparateur(input) {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 0eaaa786dbc5e6b2a92f4ac0c89188a4ac848d61
   let i: number;
   let c: number;
   let output = {};
@@ -27,23 +23,6 @@ export function comparateur(input) {
             'nbfoisrelancer' : (i + 1)
           };
           return output;
-<<<<<<< HEAD
-=======
-=======
-  console.log(input);
-  let i: number;
-  let c: number;
-  for (i = 0, c = (this.nbballs.length - 1); i <= c; i++) {
-    if (input[i] !== input[i + 1]) {
-      if ((i + 1) === (this.nbballs.length - 1)) {
-        return this.nbballs[i + 1];
-      } else {
-        if (input[i + 1] !== input[i + 2]) {
-          return this.nbballs[i + 1];
-        } else {
-          return this.nbballs[i];
->>>>>>> f6d5c293fd236d058c2f7a4008371eaa8f92b009
->>>>>>> 0eaaa786dbc5e6b2a92f4ac0c89188a4ac848d61
         }
       }
     }
